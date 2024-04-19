4d6fe47cad3f81b1bc78ab0140233a3cdb321c7b
Ameer Hamza <hamza987ameer@gmail.com>
2024-04-20 01:57:49 +0500

    initial commit

---
+++ lib/MPU6050.h
@@ -0,0 +1,23 @@
+// MPU6050 Library for ESP-32
+// Written by: Muhammad Ameer Hamza
+// Date of creation: 20-04-2024
+// Libraryb Includes Following:
+//  ---------------------------------------------------------
+//  |   Functionality                         | Description |
+//  ---------------------------------------------------------
+//  |       ReadAccel                         |Read        |
+//  |       ReadGyro                          |Read        |
+//  |       calculateRoll                     |Read        |
+//  |       calculatePitch                    |Read        |
+//  |       estimateAngleFromQuaternions      |Read        |
+//  ---------------------------------------------------------
+//   mpu.readAcceleration(ax, ay, az);
+//   mpu.readGyroscope(gx, gy, gz);
+//   roll = mpu.calculateRoll(ax, ay, az);
+//   pitch = mpu.calculatePitch(ax, ay, az);
+//   mpu.estimateAngleFromQuaternions(roll, pitch, yaw);
+//
+//
+// Repository:
+// The source code for this library can be found on GitHub/GitLab at:
+// https://github.com/ameerhamza987
